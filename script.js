43f3f2e47c24b7eedeabbb23b67e06e2b82cdd00
rjj <kachui89@gmail.com>
2026-06-16 16:35:52 +0900

    Update page to Tottenham dashboard prototype

---
+++ script.js
@@ -0,0 +1,6 @@
+const jjMemo = document.getElementById('jjMemo');
+const memoButton = document.getElementById('memoButton');
+
+memoButton.addEventListener('click', function () {
+  jjMemo.textContent = '다음 단계: 선수 목록 만들기';
+});
